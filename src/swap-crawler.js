68d80d7122ee36fdd67e725fba5137ebe3d9387d
Bakaoh <batatsar@gmail.com>
2022-03-16 18:20:04 +0700

    Update batch size

--- src/swap-crawler.js
+++ src/swap-crawler.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const Web3 = require("web3");
 
-const batchSize = 100;
+const batchSize = 200;
 const endpoint = "https://bsc-dataseed.binance.org";
 const swapTopic = '0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822';
 const syncTopic = '0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1';
