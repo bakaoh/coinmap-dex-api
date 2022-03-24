2f19c0043681d4e4b2507dc076ed87687bbe9154
Bakaoh <batatsar@gmail.com>
2022-03-24 12:16:49 +0700

    Add indexer

--- src/balance/run.js
+++ src/balance/run.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const axios = require("axios");
 const Indexer = require('./indexer');
 
@@ -5,8 +6,15 @@ const COMMON_BASE = 'http://128.199.189.253:9610';
 
 async function run() {
     const { ts, block } = (await axios.get(`${COMMON_BASE}/block/startofday?n=365`)).data;
-    const indexer = new Indexer('0x4556A6f454f15C4cD57167a62bdA65A6be325D1F');
-    await indexer.run(block);
+    let tokens = fs.readdirSync('db/transfer');
+    let c = 0;
+    console.log(`Total token: ${tokens.length}`);
+    for (let token of tokens) {
+        if (token == '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82') continue;
+        console.log(`Indexing token [${c++}] ${token}`);
+        const indexer = new Indexer(token);
+        await indexer.run(block);
+    }
 }
 
 run();
