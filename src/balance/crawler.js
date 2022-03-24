903638c1f9ba1eec8d3343476ee1aca5b3615c0e
Bakaoh <batatsar@gmail.com>
2022-03-24 17:12:50 +0700

    Crawl swap

--- src/balance/crawler.js
+++ src/balance/crawler.js
@@ -1,23 +1,52 @@
 const fs = require("fs");
-const Web3 = require("web3");
+const axios = require("axios");
 
-const endpoint = "https://bsc-dataseed.binance.org";
+const { web3, ContractAddress } = require('../utils/bsc');
+
 const transferTopic = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
-const opts = { flags: "a" };
+const swapTopic = '0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822';
 
-const web3 = new Web3(endpoint);
+const opts = { flags: "a" };
 
 const sleep = (ms) => new Promise(res => setTimeout(res, ms));
 
-const WBNB = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
-const BUSD = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56';
-const USDT = '0x55d398326f99059fF775485246999027B3197955';
-const CAKE = '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82';
-
-const IGNORE = [WBNB, BUSD, USDT, CAKE];
-
 let writer = {};
 let lastFileIdx = 0;
+
+const COMMON_BASE = 'http://128.199.189.253:9610';
+
+const cacheLP = {};
+
+async function getToken01(lpToken) {
+    if (!cacheLP[lpToken]) {
+        try {
+            const lps = (await axios.get(`${COMMON_BASE}/info/lp?a=${lpToken}`)).data;
+            cacheLP[lpToken] = lps[0];
+        } catch (err) {
+            cacheLP[lpToken] = { token0: "", token1: "" };
+        }
+    }
+    return cacheLP[lpToken];
+}
+
+async function writeSwapLog(block, txIdx, logIdx, lpToken, from, to, in0, in1, out0, out1) {
+    try {
+        const { token0, token1 } = await getToken01(lpToken);
+        if (token0 == "" || token1 == "") return;
+        const idx = Math.floor(block / 100000);
+        if (from == ContractAddress.PANCAKE_ROUTER) from = "ROUTER";
+        if (to == ContractAddress.PANCAKE_ROUTER) to = "ROUTER";
+        if (in0 == '0') {
+            // BUY TOKEN 0, SELL TOKEN 1
+            getWriter(token0, idx).write(`${block},${txIdx},${logIdx},BUY,${token1},${from},${to},${out0},${in1}\n`);
+            getWriter(token1, idx).write(`${block},${txIdx},${logIdx},SELL,${token0},${from},${to},${in1},${out0}\n`);
+        } else {
+            // SELL TOKEN 0, BUY TOKEN 1
+            getWriter(token0, idx).write(`${block},${txIdx},${logIdx},SELL,${token1},${from},${to},${in0},${out1}\n`);
+            getWriter(token1, idx).write(`${block},${txIdx},${logIdx},BUY,${token0},${from},${to},${out1},${in0}\n`);
+        }
+    } catch (err) { console.log(`Error`, block, txIdx, logIdx, lpToken, from, to, in0, in1, out0, out1, err.toString()) }
+}
 
 function getWriter(token, idx) {
     if (!writer[token]) {
@@ -39,21 +68,17 @@ async function crawlLogs(fromBlock, toBlock) {
     const pastLogs = await web3.eth.getPastLogs({
         fromBlock,
         toBlock,
-        address: CAKE,
-        topics: [transferTopic],
+        topics: [swapTopic],
     })
 
     for (let log of pastLogs) {
         try {
-            // if (IGNORE.includes(log.address)) continue;
-            if (log.topics.length != 3 || log.data == '0x') continue;
-            const fileLog = getWriter(log.address, fileIdx);
-            const value = web3.eth.abi.decodeParameters(['uint256'], log.data)
+            const values = web3.eth.abi.decodeParameters(['uint256', 'uint256', 'uint256', 'uint256'], log.data)
             const from = web3.eth.abi.decodeParameters(['address'], log.topics[1])
             const to = web3.eth.abi.decodeParameters(['address'], log.topics[2])
-            fileLog.write(`${log.blockNumber},${log.transactionIndex},${log.logIndex},${from[0]},${to[0]},${value[0].toString(10)}\n`);
+            writeSwapLog(log.blockNumber, log.transactionIndex, log.logIndex, log.address, from[0], to[0], values[0].toString(10), values[1].toString(10), values[2].toString(10), values[3].toString(10));
         } catch (err) {
-            console.log(`Decode`, log, err);
+            console.log(`Write log error`, log, err);
         }
     }
     const ms = Date.now() - startMs;
