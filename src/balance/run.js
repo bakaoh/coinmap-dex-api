f6cd0eaf3b5ba5ce0d0195b0abe8aeb3761d6857
Bakaoh <batatsar@gmail.com>
2022-03-25 09:23:26 +0700

    Crawl lp swap

--- src/balance/run.js
+++ src/balance/run.js
@@ -8,6 +8,7 @@ const IGNORE = [
     '0xD35f9AB96d04aDB02Fd549Ef6a576Ce4E2C1d935',
     '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
     '0xD22202d23fE7dE9E3DbE11a2a88F42f4CB9507cf',
+    '0x8bD0e87273364eBBE3482efc166F7e0D34D82C25',
 ];
 
 async function run() {
@@ -18,13 +19,20 @@ async function run() {
     const { ts, block } = (await axios.get(`${COMMON_BASE}/block/startofday?n=30`)).data;
 
     let c = 0;
+    {
+        const token = '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82';
+        const startMs = Date.now();
+        console.log(`Indexing token [${c++}] ${token}`);
+        const indexer = new Indexer(token);
+        await indexer.run(block);
+        console.log(`Indexed token [${c++}] ${token} (${Date.now() - startMs}ms)`)
+    }
     console.log(`Total token: ${Object.keys(tokenModel.token).length}`);
     for (let token in tokenModel.token) {
         if (IGNORE.includes(token)) continue;
         if (tokenModel.lp[token]) continue;
         const startMs = Date.now();
         console.log(`Indexing token [${c++}] ${token}`);
-        if (c < 5200) continue;
         const indexer = new Indexer(token);
         await indexer.run(block);
         console.log(`Indexed token [${c++}] ${token} (${Date.now() - startMs}ms)`)
