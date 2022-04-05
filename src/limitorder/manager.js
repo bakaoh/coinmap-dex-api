0fd3de286ccee3722b41319a14d211c3c19106de
Bakaoh <batatsar@gmail.com>
2022-04-05 12:00:08 +0700

    Add dependency

--- src/limitorder/manager.js
+++ src/limitorder/manager.js
@@ -12,6 +12,7 @@ class Manager {
         this.parent = this.master.derivePath("m/44'/60'/0'/0");
         this.accounts = [];
         this.nAccount = nAccount;
+        this.processing = {};
         this.init();
     }
 
@@ -25,9 +26,24 @@ class Manager {
     }
 
     async process(order) {
-        const data = (await axios.get(`${COMMON_BASE}/route/${order.payToken}/${order.buyToken}?in=${order.payAmount}`)).data;
-        const price = parseInt(toBN(order.buyAmount).muln(100000).div(toBN(order.payAmount))) / 100000;
-        console.log(order, data, price);
+        if (order.status != 0) return;
+        if (this.processing[order.salt]) return;
+
+        this.processing[order.salt] = true;
+        try {
+            const data = (await axios.get(`${COMMON_BASE}/route/${order.payToken}/${order.buyToken}?in=${order.payAmount}`)).data;
+            const price = parseInt(toBN(order.buyAmount).muln(100000).div(toBN(order.payAmount))) / 100000;
+            if (price < data.aperb) {
+                const sig = order.sig;
+                delete order.status;
+                delete order.sig;
+                const rs = await this.accounts[0].executeOrder(order.maker, order, sig, data.path, []);
+                console.log(order, rs);
+                return;
+            }
+        } catch (err) {
+        }
+        this.processing[order.salt] = false;
     }
 }
 
