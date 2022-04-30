85e03f80822141b244375b2dc56d5daea39cc67f
Bakaoh <batatsar@gmail.com>
2022-04-30 10:46:36 +0700

    Manual top pools

---
+++ src/manual/top-pools.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const { Partitioner } = require('../utils/io');
+const { ContractAddress, isUSD, toBN } = require('../utils/bsc');
+const SyncModel = require("../liquidity/sync");
+const PairModel = require("../liquidity/pair");
+
+const pairModel = new PairModel();
+const syncModel = new SyncModel();
+
+async function runAll() {
+    const tokens = fs.readdirSync('db/cswap');
+    console.log(`Total token: ${tokens.length}`);
+    let c = 0;
+    for (let token of tokens) {
+        const startMs = Date.now();
+        const { tokenPrice, pools } = (await syncModel.getPools(token, pairModel.getPools(token)));
+        console.log(tokenPrice, pools);
+        console.log(`Top pools ${c++} [${token}] (${Date.now() - startMs}ms)`)
+    }
+}
+
+runAll();
