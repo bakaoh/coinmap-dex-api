b74c8a4132355d44fdfe9ae7ab6b82ef66459173
Bakaoh <batatsar@gmail.com>
2022-04-30 10:54:29 +0700

    Manual top pools

--- src/manual/top-pools.js
+++ src/manual/top-pools.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
-const { Partitioner } = require('../utils/io');
-const { ContractAddress, isUSD, toBN } = require('../utils/bsc');
+const { toBN } = require('../utils/bsc');
 const SyncModel = require("../liquidity/sync");
 const PairModel = require("../liquidity/pair");
+const { getNumber } = require('../utils/format');
 
 const pairModel = new PairModel();
 const syncModel = new SyncModel();
+const writer = fs.createWriteStream(`db/pools.total`, opts)
 
 async function runAll() {
     const tokens = fs.readdirSync('db/cswap');
@@ -15,7 +16,12 @@ async function runAll() {
     for (let token of tokens) {
         const startMs = Date.now();
         const { tokenPrice, pools } = (await syncModel.getPools(token, pairModel.getPools(token)));
-        console.log(tokenPrice, pools);
+        let totalToken = toBN(0);
+        for (let pool of pools) {
+            totalToken = totalToken.add(toBN(pool.token0 == token ? pool.reserve0 : pool.reserve1));
+        }
+        const totalAmount = getNumber(totalToken.toString(10)) * tokenPrice;
+        writer.write(`${token},${totalAmount}\n`);
         console.log(`Top pools ${c++} [${token}] (${Date.now() - startMs}ms)`)
     }
 }
