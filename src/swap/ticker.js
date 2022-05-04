9e3db4f69202e3e43847c042e428076f53d3caa8
Bakaoh <batatsar@gmail.com>
2022-05-04 17:14:48 +0700

    Add /api/v1/rating/pool/:token

--- src/swap/ticker.js
+++ src/swap/ticker.js
@@ -12,9 +12,10 @@ async function getDexTrades(base, quote, resolution, countback = 500) {
     ethereum(network: bsc) {
         dexTrades(
             options: {limit: ${countback}, desc: "timeInterval.${interval}"}
+            ${exchange}
             baseCurrency: {is: "${base}"}
             quoteCurrency: {is: "${quote}"}
-            tradeAmountUsd: {gt: 1}
+            tradeAmountUsd: {gt: 10}
         ) {
             quoteAmount
             trades: count
