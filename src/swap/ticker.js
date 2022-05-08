376bf82835550a400f9aaecd2524041c1deaf79b
Bakaoh <batatsar@gmail.com>
2022-05-08 17:34:50 +0700

    Fix decimals

--- src/swap/ticker.js
+++ src/swap/ticker.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const RESOLUTION_INTERVAL = { "1": "minute", "5": "minute", "15": "minute", "60": "minute", "1D": "day", "1W": "day" };
 const RESOLUTION_COUNT = { "1": 1, "5": 5, "15": 15, "60": 60, "1D": 1, "1W": 7 };
 
-async function getDexTrades(base, quote, resolution, countback = 500) {
+async function getDexTrades(base, quote, resolution, countback = 300) {
     const interval = RESOLUTION_INTERVAL[resolution] || "minute";
     const count = RESOLUTION_COUNT[resolution] || 1;
     const exchange = `exchangeName: {is: "Pancake v2"}`;
@@ -11,21 +11,17 @@ async function getDexTrades(base, quote, resolution, countback = 500) {
 {
     ethereum(network: bsc) {
         dexTrades(
-            options: {limit: ${countback}, desc: "timeInterval.${interval}"}
-            ${exchange}
+            options: {limit: ${countback}, desc: "t.${interval}"}
             baseCurrency: {is: "${base}"}
             quoteCurrency: {is: "${quote}"}
-            tradeAmountUsd: {gt: 1}
+            tradeAmountUsd: {gt: 10}
         ) {
-            quoteAmount
-            trades: count
-            quotePrice
-            maximum_price: quotePrice(calculate: maximum)
-            minimum_price: quotePrice(calculate: minimum)
-            open_price: minimum(of: block, get: quote_price)
-            close_price: maximum(of: block, get: quote_price)
-            baseAmount
-            timeInterval {
+            h: quotePrice(calculate: maximum)
+            l: quotePrice(calculate: minimum)
+            o: minimum(of: block, get: quote_price)
+            c: maximum(of: block, get: quote_price)
+            v: baseAmount
+            t: timeInterval {
                 ${interval}(count: ${count})
             }
         }
@@ -44,12 +40,12 @@ async function getDexTrades(base, quote, resolution, countback = 500) {
     });
     const t = [], c = [], o = [], h = [], l = [], v = [];
     res.data.data.ethereum.dexTrades.forEach(item => {
-        t.push(Math.round(new Date(item.timeInterval[interval]) / 1000));
-        c.push(item.close_price);
-        o.push(item.open_price);
-        h.push(item.maximum_price);
-        l.push(item.minimum_price);
-        v.push(item.baseAmount);
+        t.push(Math.round(new Date(item.t[interval]) / 1000));
+        c.push(item.c);
+        o.push(item.o);
+        h.push(item.h);
+        l.push(item.l);
+        v.push(item.v);
     });
     return { s: "ok", t, c, o, h, l, v };
 }
