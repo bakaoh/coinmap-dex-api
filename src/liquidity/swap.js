5ecd675ef0c3ca5a8cfcd5d84da081801ea66320
Bakaoh <batatsar@gmail.com>
2022-05-17 15:38:13 +0700

    Test ticker

--- src/liquidity/swap.js
+++ src/liquidity/swap.js
@@ -177,6 +177,7 @@ class SwapModel {
                     }
 
                     v = v.add(toBN(amount0));
+                    if (amount0.length < decimals) return;
                     const price = this.calcPrice([amount0, amountUSD], decimals);
                     if (price == 0) return;
                     c = price;
