e22b27054e9d423bfb71a0cdccb264183668ed31
Bakaoh <batatsar@gmail.com>
2022-06-14 00:07:36 +0700

    Update sync

--- src/liquidity/sync.js
+++ src/liquidity/sync.js
@@ -1,6 +1,7 @@
 const Crawler = require("../utils/crawler");
 const { web3, ContractAddress, isUSD, toBN } = require('../utils/bsc');
 const { Partitioner, getLastLine, getLastFile, getLastFiles } = require('../utils/io');
+const { getNumber } = require('../utils/format');
 
 const SYNC_TOPIC = '0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1';
 const BLOCK_FILE = 'logs/sync.block';
@@ -124,6 +125,7 @@ class SyncModel {
             const ts = Math.floor((block - fromBlock) / blockInterval) * blockInterval * 3 + fromTs;
             updateRs(ts, tick);
         }
+        for (let tick of rs) tick.v = getNumber(tick.v);
         return rs;
     }
 
