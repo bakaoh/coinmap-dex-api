2e6f3f18e8357dd1e9aae8aa100cd6461b0c0087
Bakaoh <batatsar@gmail.com>
2022-06-13 09:19:40 +0700

    Update search

--- src/balance/token.js
+++ src/balance/token.js
@@ -52,7 +52,7 @@ class TokenModel {
     searchToken(text) {
         if (text.length < 3) return [];
         const address = getAddress(text);
-        if (this.token[address]) return [this.token[address]];
+        if (this.token[address]) return [{ address, decimals: this.token[address][0], symbol: this.token[address][1], name: this.token[address][2] }];
         return this.indexer.search(text);
     }
 
